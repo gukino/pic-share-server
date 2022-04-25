ec7cca6c260344a7116358d845ab6bbeff7e3ae6
Victory <47774751+victory66666666@users.noreply.github.com>
2022-04-26 00:29:55 +0800

    改名

--- pic-share/src/main/java/hku/picshare/service/Impl/PhotoServiceImpl.java
+++ pic-share/src/main/java/hku/picshare/service/Impl/PhotoServiceImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
  *
  */
 @Service
-public class photoServiceImpl extends ServiceImpl<photoMapper, photo> implements PhotoService {
+public class PhotoServiceImpl extends ServiceImpl<photoMapper, photo> implements PhotoService {
 
 
 
--- pic-share/src/main/java/hku/picshare/service/Impl/TagServiceImpl.java
+++ pic-share/src/main/java/hku/picshare/service/Impl/TagServiceImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
  *
  */
 @Service
-public class tagServiceImpl extends ServiceImpl<tagMapper, tag> implements TagService {
+public class TagServiceImpl extends ServiceImpl<tagMapper, tag> implements TagService {
 
 
 
