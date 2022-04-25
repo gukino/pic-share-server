ec96ee559429e4fca0d3b58af98c64541b05cbfc
Victory <47774751+victory66666666@users.noreply.github.com>
2022-04-25 23:40:47 +0800

    搜索功能添加

---
+++ pic-share/src/main/java/hku/picshare/controller/SearchController.java
@@ -0,0 +1,14 @@
+package hku.picshare.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author yhp
+ * @create 2022-04-25 23:37
+ */
+
+@RestController
+public class SearchController {
+
+
+}
